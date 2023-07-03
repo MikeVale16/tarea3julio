b477aefb6579f56b680c8e90a15d2c1496e09954
MikeVale16 <mikevale45130@gmail.com>
2023-07-02 20:29:15 -0600

    ejercicio 6 completo

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,2 +1,20 @@
 # Escribe una función que genere una lista con los números de la serie de fibonacci. La función debe recibir la cantidad de números a generar
 
+
+def fibonnaci(num):
+    num1=0
+    num2=1
+    resultado=0
+    print(num1)
+    for i in range(0,num):
+    
+        resultado=num1+num2
+        print(resultado)
+        num1=num2
+        num2=resultado
+    return "Success"
+
+iteration= int(input("Por favor dime cuantos elementos de fibonacci quieres ver?\n"))
+print("\n\n\n\n\n")
+
+print(fibonnaci(iteration))
